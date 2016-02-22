4b3ab8f177fb236ad519c0b54f1f050c42e93627
Davideryu Orihara <davideryu_orihara@dwango.co.jp>
2016-02-22 19:42:24 +0900

    コード中の typo 修正 secondImtem → secondItem

--- index.js
+++ index.js
@@ -30,19 +30,19 @@ let server = http.createServer(basic, (req, res) => {
         res.write(jade.renderFile('./form.jade', {
           path: req.url,
           firstItem: '焼き肉',
-          secondImtem: 'しゃぶしゃぶ'
+          secondItem: 'しゃぶしゃぶ'
         }));
       } else if (req.url === '/enquetes/rice-bread') {
         res.write(jade.renderFile('./form.jade', {
           path: req.url,
           firstItem: 'ごはん',
-          secondImtem: 'パン'
+          secondItem: 'パン'
         }));
       } else if (req.url === '/enquetes/sushi-pizza') {
         res.write(jade.renderFile('./form.jade', {
           path: req.url,
           firstItem: '寿司',
-          secondImtem: 'ピザ'
+          secondItem: 'ピザ'
         }));
       }
       res.end();
