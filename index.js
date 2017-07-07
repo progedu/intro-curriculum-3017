cfa7d324d0a3e6794daac63ae62d8ea1239214e3
zana-ishtar <yoshiki_17n1300022@nnn.ed.jp>
2017-07-07 06:30:01 +0000

    ログアウトリンクの追加

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const auth = require('http-auth');
 const basic = auth.basic(
   { realm: 'Enter username and password.' },
   (username, password, callback) => {
-    callback(username === 'guest' && password === 'xaXZJQmE');
+    callback(username === 'guest' && password === 'kanryaku');
   });
 const server = http.createServer(basic, (req, res) => {
   console.info('Requested by ' + req.connection.remoteAddress);
