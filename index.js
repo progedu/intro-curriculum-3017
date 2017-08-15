89d860071f021bba38109ae32ccf920cf53e902c
yusuke_nakajima <www.yyworld@gmail.com>
2017-08-15 11:52:08 +0900

    コード整形

--- index.js
+++ index.js
@@ -3,67 +3,67 @@ const http = require('http');
 const jade = require('jade');
 const auth = require('http-auth');
 const basic = auth.basic(
-  { realm: 'Enter username and password.' },
-  (username, password, callback) => {
-    callback(username === 'guest' && password === 'xaXZJQmE');
-  });
-const server = http.createServer(basic, (req, res) => {
-  console.info('Requested by ' + req.connection.remoteAddress);
-    
-  if (req.url === '/logout') {
-    res.writeHead(401, {
-      'Content-Type': 'text/plain; charset=utf-8'
+    {realm: 'Enter username and password.'},
+    (username, password, callback) => {
+        callback(username === 'guest' && password === 'xaXZJQmE');
     });
-    res.end('ログアウトしました');
-    return;
-  }
-  
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  });
+const server = http.createServer(basic, (req, res) => {
+    console.info('Requested by ' + req.connection.remoteAddress);
 
-  switch (req.method) {
-    case 'GET':
-      if (req.url === '/enquetes/yaki-shabu') {
-        res.write(jade.renderFile('./form.jade', {
-          path: req.url,
-          firstItem: '焼き肉',
-          secondItem: 'しゃぶしゃぶ'
-        }));
-      } else if (req.url === '/enquetes/rice-bread') {
-        res.write(jade.renderFile('./form.jade', {
-          path: req.url,
-          firstItem: 'ごはん',
-          secondItem: 'パン'
-        }));
-      } else if (req.url === '/enquetes/sushi-pizza') {
-        res.write(jade.renderFile('./form.jade', {
-          path: req.url,
-          firstItem: '寿司',
-          secondItem: 'ピザ'
-        }));
-      }
-      res.end();
-      break;
-    case 'POST':
-      req.on('data', (data) => {
-        const decoded = decodeURIComponent(data);
-        console.info('投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="jp"><head><meta charset="utf-8"></head><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
-        res.end();
-      });
-      break;
-    default:
-      break;
-  }
+    if (req.url === '/logout') {
+        res.writeHead(401, {
+            'Content-Type': 'text/plain; charset=utf-8'
+        });
+        res.end('ログアウトしました');
+        return;
+    }
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    });
+
+    switch (req.method) {
+        case 'GET':
+            if (req.url === '/enquetes/yaki-shabu') {
+                res.write(jade.renderFile('./form.jade', {
+                    path: req.url,
+                    firstItem: '焼き肉',
+                    secondItem: 'しゃぶしゃぶ'
+                }));
+            } else if (req.url === '/enquetes/rice-bread') {
+                res.write(jade.renderFile('./form.jade', {
+                    path: req.url,
+                    firstItem: 'ごはん',
+                    secondItem: 'パン'
+                }));
+            } else if (req.url === '/enquetes/sushi-pizza') {
+                res.write(jade.renderFile('./form.jade', {
+                    path: req.url,
+                    firstItem: '寿司',
+                    secondItem: 'ピザ'
+                }));
+            }
+            res.end();
+            break;
+        case 'POST':
+            req.on('data', (data) => {
+                const decoded = decodeURIComponent(data);
+                console.info('投稿: ' + decoded);
+                res.write('<!DOCTYPE html><html lang="jp"><head><meta charset="utf-8"></head><body><h1>' +
+                    decoded + 'が投稿されました</h1></body></html>');
+                res.end();
+            });
+            break;
+        default:
+            break;
+    }
 
 }).on('error', (e) => {
-  console.error('Server Error', e);
+    console.error('Server Error', e);
 }).on('clientError', (e) => {
-  console.error('Client Error', e);
+    console.error('Client Error', e);
 });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+    console.info('Listening on ' + port);
 });
