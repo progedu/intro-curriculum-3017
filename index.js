59d16ed566fb0ffba2e2b85b55ab1b895ab5cdf6
tpky31092 <tpky31092@gmail.com>
2020-02-20 06:01:45 +0900

    認証で利用者を制限_ログアウトリンクを追加

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const server = http.createServer(basic, (req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info(' 投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
