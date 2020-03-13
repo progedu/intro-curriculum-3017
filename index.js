7a5714b574c54f3e4dfb4fded584ed5c4a28bec7
stellar-mouse <s_mouse@amigo2.ne.jp>
2020-03-13 18:50:05 +0900

    logoutリンク追加

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const server = http.createServer(basic, (req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info('投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
@@ -65,7 +65,7 @@ const server = http.createServer(basic, (req, res) => {
 }).on('clientError', (e) => {
   console.error('Client Error', e);
 });
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
