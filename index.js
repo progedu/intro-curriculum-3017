4d3d1661f94cd2cb25f12213fe5d4fd7285dad53
Satoshi Niikura <niikura0703@gmail.com>
2020-04-23 16:05:29 +0900

    18.認証の回答

--- index.js
+++ index.js
@@ -52,9 +52,12 @@ const server = http.createServer(basic, (req, res) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         const decoded = decodeURIComponent(rawData);
+        const qs = require('querystring');
+        const answer = qs.parse(decoded);
         console.info('[' + now + '] 投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+          answer['name'] + 'さんは、' + answer['favorite'] +
+          'に投票しました。</h1></body></html>');
         res.end();
       });
       break;
