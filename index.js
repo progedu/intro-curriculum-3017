4a5ae2eece4c1813513a81148ed17b03d5f5011c
Harumi <hru.o6o4@gmail.com>
2020-06-17 12:52:02 +0900

    ログアウトボタンの実装]

--- index.js
+++ index.js
@@ -1,72 +1,79 @@
-'use strict';
-const http = require('http');
-const pug = require('pug');
-const auth = require('http-auth');
+"use strict";
+const http = require("http");
+const pug = require("pug");
+const auth = require("http-auth");
 const basic = auth.basic(
-  { realm: 'Enquetes Area.' },
+  { realm: "Enquetes Area." },
   (username, password, callback) => {
-    callback(username === 'guest' && password === 'xaXZJQmE');
-  });
-const server = http.createServer(basic, (req, res) => {
-  const now = new Date();
-  console.info('Requested by ' + req.connection.remoteAddress);
-
-  if (req.url === '/logout') {
-    res.writeHead(401, {
-      'Content-Type': 'text/plain; charset=utf-8'
-    });
-    res.end('ログアウトしました');
-    return;
+    callback(username === "guest" && password === "mHkns5Ad");
   }
+);
+const server = http
+  .createServer(basic, (req, res) => {
+    console.info("Requested by " + req.connection.remoteAddress);
 
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  });
-
-  switch (req.method) {
-    case 'GET':
-      if (req.url === '/enquetes/yaki-shabu') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: '焼き肉',
-          secondItem: 'しゃぶしゃぶ'
-        }));
-      } else if (req.url === '/enquetes/rice-bread') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: 'ごはん',
-          secondItem: 'パン'
-        }));
-      } else if (req.url === '/enquetes/sushi-pizza') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: '寿司',
-          secondItem: 'ピザ'
-        }));
-      }
-      res.end();
-      break;
-    case 'POST':
-      let rawData = '';
-      req.on('data', (chunk) => {
-        rawData = rawData + chunk;
-      }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
-        res.end();
+    if (req.url === "/logout") {
+      res.writeHead(401, {
+        "Content-Type": "text/plain; charset=utf-8",
       });
-      break;
-    default:
-      break;
-  }
-}).on('error', (e) => {
-  console.error('Server Error', e);
-}).on('clientError', (e) => {
-  console.error('Client Error', e);
-});
+      res.end("ログアウトしました");
+      return;
+    }
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8",
+    });
+
+    switch (req.method) {
+      case "GET":
+        let firstItem = "ごはん";
+        let secondItem = "パン";
+        if (req.url === "/enquetes/ham-steak") {
+          firstItem = "ハンバーグ";
+          secondItem = "ステーキ";
+        } else if (req.url === "/enquetes/pasta-noodle") {
+          firstItem = "パスタ";
+          secondItem = "ラーメン";
+        } else if (req.url === "/enquetes/sushi-pizza") {
+          firstItem = "寿司";
+          secondItem = "ピザ";
+        }
+        res.write(
+          pug.renderFile("./form.pug", {
+            path: req.url,
+            firstItem: firstItem,
+            secondItem: secondItem,
+          })
+        );
+        res.end();
+        break;
+      case "POST":
+        let rawData = "";
+        req
+          .on("data", (chunk) => {
+            rawData = rawData + chunk;
+          })
+          .on("end", () => {
+            const decoded = decodeURIComponent(rawData);
+            console.info("投稿: " + decoded);
+            res.write(
+              '<!DOCTYPE html><html lang="ja"><body><h1>' +
+                decoded +
+                "が投稿されました</h1></body></html>"
+            );
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on("error", (e) => {
+    console.error("Server Error", e);
+  })
+  .on("clientError", (e) => {
+    console.error("Client Error", e);
+  });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+  console.info("Listening on " + port);
 });
